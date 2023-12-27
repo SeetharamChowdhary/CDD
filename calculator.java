da3318e2ec7ef50c5feee56437bc6f7dbe4991e9
Seetharam <ramchowdhary73@gmail.com>
2023-12-27 12:23:38 +0530

    added calculator program in terminal itself

---
+++ calculator.java
@@ -0,0 +1,15 @@
+import java.util*
+public class Main
+{
+	public static void main(String Args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=a+b;
+		int d=a-b;
+		int e=a*b;
+		int f=a/b;
+		System.out.println(c+" "+d+" "+"e"+" "+f);
+	}
+}
--- helloworld.java
+++
@@ -1,4 +0,0 @@
-
-hii how are you i am fine 
-my name is  lk.Seetharam chowdhary i am studying in kl university pursuing my carrier in computer science degree and i have opted for software modelling and devops specilaization
-
